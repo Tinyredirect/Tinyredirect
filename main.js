01061cab12bbaf43d985546a2fc3f02e16e95c25
Tinyredirect <tinyredirect0@gmail.com>
2025-04-15 11:57:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ async function connectWallet() {
 
     await db.ref("wallets/" + currentWalletAddress).set({ address: currentWalletAddress });
 
-    const userTipLink = `${window.location.origin}/tip.html?to=${nicknameInput.value.trim()}`;
+    const userTipLink = `${window.location.origin}/tip.html?to=${nicknameInput.value}`;
     tipLink.value = userTipLink;
 
     connectBtn.textContent = "Connected";
